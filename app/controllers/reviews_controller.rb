7a0d2c749aa20326f1ce68552308d1b5c58dd589
Zakir Hussain <zack.mesacc@gmail.com>
2022-02-24 15:53:34 -0600

    Created many to many relationship between user and movies with reviews as join

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+    before_action :require_signin
     before_action :set_movie
      
     def index 
@@ -11,6 +12,7 @@ class ReviewsController < ApplicationController
 
     def create 
         @review = @movie.reviews.new(review_params)
+        @review.user = current_user 
 
         if @review.save 
             redirect_to movie_reviews_url(@movie), notice: 'Thanks for the review!'
@@ -25,6 +27,6 @@ class ReviewsController < ApplicationController
         end
 
         def review_params 
-            params.require(:review).permit(:name, :stars, :comment)
+            params.require(:review).permit(:stars, :comment)
         end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord 
     has_secure_password
 
+    has_many :reviews, dependent: :destroy 
+
     validates :name, presence: true 
     validates :email, presence: true, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 6, allow_blank: true }
