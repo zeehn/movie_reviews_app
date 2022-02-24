8dc4c887334227deee8b146dd68fdb8ae9707e42
Zakir Hussain <zack.mesacc@gmail.com>
2022-02-23 23:14:26 -0600

    Added admin role to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController 
+    before_action :require_signin, except: [:new, :create]
+    before_action :require_correct_user, only: [:edit, :update] 
+    before_action :require_admin, only: [:destroy]
+
     def index 
         @users = User.all
     end
@@ -23,12 +27,9 @@ class UsersController < ApplicationController
     end
 
     def edit 
-        @user = User.find(params[:id])
     end
 
     def update 
-        @user = User.find(params[:id])
-
         if @user.update(user_params)
             redirect_to @user, notice: "Account successfully updated!"
         else
@@ -37,10 +38,7 @@ class UsersController < ApplicationController
     end
 
     def destroy 
-        @user = User.find(params[:id])
-
         if @user.destroy 
-            session[:user_id] = nil 
             redirect_to movies_url, alert: 'Account successfully delelted!'
         end
     end
@@ -48,5 +46,11 @@ class UsersController < ApplicationController
     private 
         def user_params 
             params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+        end
+
+        def require_correct_user  
+            @user = User.find(params[:id])
+
+            redirect_to movies_url, alert: 'Not authorized!' unless current_user?(@user) 
         end
 end
