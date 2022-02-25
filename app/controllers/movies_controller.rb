1f7cf03c7624fcaf06bab1d77a53ead9e145dd32
Zakir Hussain <zack.mesacc@gmail.com>
2022-02-25 15:24:01 -0600

    Added slug to movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController 
     before_action :require_signin, except: [:index, :show] 
     before_action :require_admin, except: [:index, :show]
+    before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
     def index 
         @movies = Movie.send(movies_filter)
@@ -20,7 +21,6 @@ class MoviesController < ApplicationController
     end
 
     def show 
-        @movie = Movie.find(params[:id])
         @fans = @movie.fans 
         @genres = @movie.genres.order(:name)
         if current_user 
@@ -29,11 +29,9 @@ class MoviesController < ApplicationController
     end
 
     def edit 
-        @movie = Movie.find(params[:id])
     end
 
     def update 
-        @movie = Movie.find(params[:id])
         if @movie.update(movie_params)
             redirect_to @movie, notice: 'Movie updated successfully!' 
         else
@@ -42,13 +40,16 @@ class MoviesController < ApplicationController
     end
 
     def destroy 
-        @movie = Movie.find(params[:id])
         if @movie.destroy 
             redirect_to movies_url, alert: 'Movie deleted successfully.' 
         end
     end
 
     private 
+        def set_movie 
+            @movie = Movie.find_by!(slug: params[:id])
+        end
+
         def movie_params
             params.require(:movie).permit(:title, :description, :total_gross, :rating, :released_on, 
                                           :director, :duration, :image_file_name, genre_ids: [] )
